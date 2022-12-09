436d1d03231eb599e81fd1f3a8684014976fedba
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-12-09 16:16:02 -0500

    smoothing and comments

--- py/qcfitter/mathtools.py
+++ py/qcfitter/mathtools.py
@@ -53,3 +53,38 @@ class Fast1DInterpolator(object):
 
     def __call__(self, x):
         return _fast_interp1d(x, self.xp0, self.dxp, self.fp)
+
+# ===================================================
+
+def get_smooth_ivar(ivar, sigma_pix=20, pad_size=25, esigma=3.5):
+    error = np.empty_like(ivar)
+    w1 = ivar > 0
+    error[w1] = 1/np.sqrt(ivar)
+    median_err = np.median(error[w1])
+    error[~w1] = median_err
+
+    # Isolate high noise pixels
+    mad = np.median(np.abs(error - median_err))*1.4826
+    w2 = (error - median_err) > esigma*mad
+    err_org = error[w2].copy()
+
+    # Replace them with the median
+    error[w2] = median_err
+
+    # Pad the input array to get rid of annoying edge effects
+    # Pad values are set to the edge value
+    arrsize    = self.size+2*pad_size
+    padded_arr = np.pad(error, pad_size, mode='edge')
+
+    kvals     = np.fft.rfftfreq(arrsize)
+    smerror_k = np.fft.rfft(padded_arr)*np.exp(-(kvals*sigma_pix)**2/2.)
+
+    error = np.fft.irfft(smerror_k, n=arrsize)[pad_size:-pad_size]
+
+    # Restore values of bad pixels
+    error[w2] = err_org
+    ivar2 = 1/error**2
+    ivar2[~w1] = 0
+
+    return ivar2
+
