3adc43f68c7ac5c40336c784e13d47654e63e094
Naim Goksel Karacayli <naimgoksel.karacayli@yale.edu>
2022-12-22 10:05:06 -0500

    fix spectrum for catalog

--- py/qcfitter/spectrum.py
+++ py/qcfitter/spectrum.py
@@ -274,7 +274,7 @@ class Spectrum(object):
                 if Spectrum._dwave is None:
                     Spectrum._dwave = wave[arm][1] - wave[arm][0]
 
-    def __init__(self, catrow, z_qso, targetid, ra, dec, wave, flux, ivar, mask, reso, idx):
+    def __init__(self, catrow, wave, flux, ivar, mask, reso, idx):
         self.catrow = catrow
         Spectrum._set_wave(wave)
 
