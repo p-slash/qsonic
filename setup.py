a6eb58c69ca5aa7382e5357cc5fd11fbdce219cc
Naim Goksel Karacayli <ngokselk@gmail.com>
2023-03-14 14:01:05 -0400

    setup cfg and pytoml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import setup, find_namespace_packages
-import os
+from setuptools import setup
 
-binscripts = [os.path.join("bin", f) for f in os.listdir("bin")
-              if not f.startswith(".")]
-with open("requirements.txt") as file_reqs:
-    requirements = file_reqs.read().splitlines()
-
-setup(
-    name="qsonic",
-    version="1.0",
-    packages=find_namespace_packages(where='py'),
-    package_dir={'': 'py/'},
-    scripts=binscripts,
-    install_requires=requirements,
-
-    # metadata to display on PyPI
-    author="Naim Goksel Karacayli",
-    author_email="ngokselk@gmail.com",
-    description=("Quasar continuum fitter for DESI."),
-    # could also include long_description, download_url, etc.
-)
+setup()
