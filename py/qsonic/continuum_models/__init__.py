6d0ee8533e51e3af0f668099e49c59a943eda5a2
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-05-09 14:47:37 -0400

    add init.py to load all continuum_models by `import qsonic.continuum_models`

---
+++ py/qsonic/continuum_models/__init__.py
@@ -0,0 +1,9 @@
+from .base_continuum_model import BaseContinuumModel
+from .input_continuum_model import InputContinuumModel
+from .picca_continuum_model import PiccaContinuumModel
+from .true_continuum_model import TrueContinuumModel
+
+__all__ = [
+    "BaseContinuumModel", "PiccaContinuumModel", "InputContinuumModel",
+    "TrueContinuumModel"
+]
